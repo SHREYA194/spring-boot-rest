f1234ecec907968fb7087a776303c2ff92df258a
SHREYA194 <shreyalad1904@gmail.com>
2025-06-27 20:16:38 +0530

    Spring Security added

---
+++ src/main/java/com/project/spring_boot_rest/service/UserService.java
@@ -0,0 +1,21 @@
+package com.project.spring_boot_rest.service;
+
+import com.project.spring_boot_rest.model.User;
+import com.project.spring_boot_rest.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public User saveUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
+}
